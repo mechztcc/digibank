ca37226d85d06743d254ddea99f4e9afd1a0e11b
mechztcc <dojathan@gmail.com>
2023-05-17 09:51:30 -0300

    feat: gradient on welcome page

--- lib/app/modules/home/pages/welcome_page.dart
+++ lib/app/modules/home/pages/welcome_page.dart
@@ -12,89 +12,103 @@ class WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Image(image: AssetImage('assets/images/welcome.png')),
-            const Text(
-              'Bem vindo ao Digibank',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10),
-              child: const Text(
-                'Acesse sua conta de forma segura e prática para desfrutar dos nossos serviços financeiros.',
-                style: TextStyle(fontSize: 20),
-                textAlign: TextAlign.center,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 253, 253, 254),
+              Color.fromARGB(255, 228, 189, 236),
+            ],
+            begin: FractionalOffset(0.0, 0.0),
+            end: FractionalOffset(0.0, 1.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Image(image: AssetImage('assets/images/welcome.png')),
+              const Text(
+                'Bem vindo ao Digibank',
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
               ),
-            ),
-            Container(
-                margin: const EdgeInsets.symmetric(vertical: 30),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          padding: MaterialStateProperty.all<EdgeInsets>(
-                            const EdgeInsets.symmetric(
-                              vertical: 15,
-                            ),
-                          ),
-                        ),
-                        onPressed: () {
-                          Modular.to.navigate('/auth');
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: const [
-                            Text(
-                              'Sou novo por aqui',
-                              style: TextStyle(
-                                fontSize: 20,
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                child: const Text(
+                  'Acesse sua conta de forma segura e prática para desfrutar dos nossos serviços financeiros.',
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                  margin: const EdgeInsets.symmetric(vertical: 30),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            padding: MaterialStateProperty.all<EdgeInsets>(
+                              const EdgeInsets.symmetric(
+                                vertical: 15,
                               ),
                             ),
-                            Icon(Icons.new_label),
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          padding: MaterialStateProperty.all<EdgeInsets>(
-                            const EdgeInsets.symmetric(
-                              vertical: 15,
-                            ),
+                          ),
+                          onPressed: () {
+                            Modular.to.navigate('/auth');
+                          },
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: const [
+                              Text(
+                                'Sou novo por aqui',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Icon(Icons.new_label),
+                            ],
                           ),
                         ),
-                        onPressed: () {
-                          Modular.to.navigate('/auth/login');
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: const [
-                            Text(
-                              'Acessar minha conta',
-                              style: TextStyle(
-                                fontSize: 20,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            padding: MaterialStateProperty.all<EdgeInsets>(
+                              const EdgeInsets.symmetric(
+                                vertical: 15,
                               ),
                             ),
-                            Icon(Icons.fingerprint)
-                          ],
+                          ),
+                          onPressed: () {
+                            Modular.to.navigate('/auth/login');
+                          },
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: const [
+                              Text(
+                                'Acessar minha conta',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Icon(Icons.fingerprint)
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ))
-          ],
+                    ],
+                  ))
+            ],
+          ),
         ),
       ),
     );
