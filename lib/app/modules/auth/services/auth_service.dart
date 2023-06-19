c20510cf015f0bb9430faacc4fe325aa8137b892
mechztcc <dojathan@gmail.com>
2023-06-19 16:00:07 -0300

    feat: create session with success

--- lib/app/modules/auth/services/auth_service.dart
+++ lib/app/modules/auth/services/auth_service.dart
@@ -1,5 +1,7 @@
 import 'package:digibank/app/modules/auth/models/UserModel.dart';
 import 'package:digibank/app/modules/auth/repositories/auth_repository.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   final AuthRepository _repository;
@@ -32,7 +34,11 @@ class AuthService {
         document,
         password,
       );
-      print(token);
+
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('token', token);
+
+      Modular.to.navigate('/home');
     } catch (e) {
       rethrow;
     }
